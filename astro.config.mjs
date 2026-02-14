6864d8954a2b161c5fb6b323fe79be72ab383502
santiago <96703054+santilliMV@users.noreply.github.com>
2026-02-13 20:44:54 -0500

    Update Astro configuration
    Removed edgeoneAdapter and updated site and base properties.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import edgeoneAdapter from '@edgeone/astro';
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: edgeoneAdapter(),
   output: 'static',
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://santillimv.github.io',
+  base: '/',
 });
